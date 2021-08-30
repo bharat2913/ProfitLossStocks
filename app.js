ff6b5befff0c5dd40363d920dc946e16a34fc644
bharat2913 <bs987143@gmail.com>
2021-08-30 14:40:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var initialPrice = document.querySelector('#initialPrice');
+var stocksQuantity = document.querySelector('#stocksQuantity');
+var currentPrice = document.querySelector('#currentPrice');
+var btn = document.querySelector('#btn');
+var output = document.querySelector('#output');
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    // Loss
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+    output.classList.add('loss');
+    output.classList.remove('profit');
+    output.classList.remove('noGain');
+
+    showOutput(
+      `Hey! The loss is ${loss} and the percent is ${lossPercentage.toFixed(2)}`
+    );
+  } else if (current > initial) {
+    // Profit
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+    showOutput(
+      `YAY! The profit is ${profit} and the percent is ${profitPercentage.toFixed(
+        2
+      )}`
+    );
+    output.classList.add('profit');
+    output.classList.remove('loss');
+    output.classList.remove('noGain');
+  } else {
+    // rest
+    showOutput(`No pain NO gain, No gain No Pain`);
+  }
+}
+
+function showOutput(message) {
+  output.innerText = message;
+}
+
+btn.addEventListener('click', () => {
+  var ip = initialPrice.value;
+  var qty = stocksQuantity.value;
+  var cp = currentPrice.value;
+  calculateProfitAndLoss(ip, qty, cp);
+});
